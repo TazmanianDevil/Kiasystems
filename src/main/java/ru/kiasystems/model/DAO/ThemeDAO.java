bef9fb940c021433b26d239d8dfa518f347011d6
TazmanianDevil <musinmh@gmail.com>
2016-03-09 00:25:09 +0300

    xsd

--- src/main/java/ru/kiasystems/model/DAO/ThemeDAO.java
+++ src/main/java/ru/kiasystems/model/DAO/ThemeDAO.java
@@ -3,6 +3,6 @@ package ru.kiasystems.model.DAO;
 import ru.kiasystems.model.entity.Theme;
 import java.util.Collection;
 interface ThemeDAO {
-    Collection getAllThemes();
+    Collection getAllThemes();zx
 
 }
