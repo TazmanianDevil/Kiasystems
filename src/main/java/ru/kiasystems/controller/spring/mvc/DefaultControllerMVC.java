8c183b5e4e5f378bb182cb312d8245330274284c
TazmanianDevil <musinmh@gmail.com>
2016-05-19 16:52:03 +0300

    Прикрутил JSP'шки и адекватное отображение стилей и картинок.

---
+++ src/main/java/ru/kiasystems/controller/spring/mvc/DefaultControllerMVC.java
@@ -0,0 +1,15 @@
+package ru.kiasystems.controller.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class DefaultControllerMVC {
+    @RequestMapping(method = RequestMethod.GET)
+    public String showWelcome(Model uiModel) {
+        return "default";
+    }
+}
